4c0f081ebb1b16e205d90c2dd373151c9b8afb52
scorch116 <e.dakin@hotmail.com>
2022-09-27 16:51:41 +0100

    DB connection test done

--- app.py
+++ app.py
@@ -1,1 +1,2 @@
 #Main applicaiton 
+print("hello world")
--- database_service.py
+++ database_service.py
@@ -1,3 +1,18 @@
 #Create helper funcitons to communicate with the database
 #Cloud data base
 #database1-scorchdb116.harperdbcloud.com
+
+import harperdb
+#URL of cloud DB
+url = "https://database1-scorchdb116.harperdbcloud.com"
+username = "Admin"
+password = "Cooldude00!!"
+
+#Connection to harperDB
+db = harperdb.HarperDB(
+    url=url,
+    username=username,
+    password=password
+)
+
+print(db.describe_all())
